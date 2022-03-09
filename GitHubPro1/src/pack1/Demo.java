2fe1cf66955e5173d2df74baf188e08e9b5e2647
Kumbhar, Suyog <kumbhar.suyog@principal.com>
2022-03-09 11:48:45 +0530

    Java project has been created with simple implementation of MyInterface

---
+++ GitHubPro1/src/pack1/Demo.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import pack1.MyInterfaceImpl;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInterfaceImpl impl=new MyInterfaceImpl();
+		impl.disp();
+	}
+
+}
---
+++ GitHubPro1/src/pack1/MyInterfaceImpl.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class MyInterfaceImpl implements MyInterface{
+	//@Override
+	public void disp() {
+		System.out.println("Inside MyInterfaceImpl class");
+	}
+}
